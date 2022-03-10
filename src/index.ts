422b3f98cf77b5c6ab49c494e89d76d8d3b0c03a
PK Wadsworth <pkwads2@gmail.com>
2022-03-10 21:08:20 +1300

    Added credential saving and made all versions download

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@
 
 import inquirer from 'inquirer';
 import path from 'path';
+import fs from 'fs';
+
 import { getProjects } from './getProjects';
 import { getProjectVersion } from './getProjectVersion';
 import { getToken } from './getToken';
@@ -11,20 +13,48 @@ async function main() {
 	//INTRO
 	console.log('Welcome to the ACP CLI');
 
-	const loginDetailsAnswers: { UPI: string; password: string } =
-		await inquirer.prompt([
+	var upi: string | null = null;
+	var password: string | null = null;
+
+	if (fs.existsSync('./credentials.json')) {
+		const credentials = JSON.parse(
+			fs.readFileSync('./credentials.json', 'utf8').toString()
+		);
+
+		upi = credentials.UPI ?? null;
+		password = credentials.password ?? null;
+	} else {
+		const loginDetailsAnswers: {
+			UPI: string;
+			password: string;
+			remember: boolean;
+		} = await inquirer.prompt([
 			{ name: 'UPI', type: 'input' },
 			{ name: 'password', type: 'password' },
+			{ name: 'remember', type: 'confirm', message: 'Remember credentials?' },
 		]);
 
-	const username = loginDetailsAnswers.UPI + '@aucklanduni.ac.nz';
-	const password = loginDetailsAnswers.password;
+		if (loginDetailsAnswers.remember) {
+			fs.writeFileSync(
+				'./credentials.json',
+				JSON.stringify({
+					UPI: loginDetailsAnswers.UPI,
+					password: loginDetailsAnswers.password,
+				})
+			);
+		}
+
+		upi = loginDetailsAnswers.UPI;
+		password = loginDetailsAnswers.password;
+	}
+
+	if (!upi || !password) {
+		console.log('Invalid credentials');
+		return;
+	}
+	const username = upi + '@aucklanduni.ac.nz';
 
 	const token = await getToken(username, password);
-
-	const basePath = './ACP';
-
-	// console.log('Got a token', token);
 
 	if (!token) {
 		console.log('There was a login failure, sorry :(');
@@ -50,8 +80,15 @@ async function main() {
 				value: p.id,
 			})),
 		},
+		{
+			type: 'input',
+			name: 'savedir',
+			message: 'Where would you like to save the project(s)?',
+			default: './ACP',
+		},
 	]);
 
+	const basePath: string = selectedProjectAnswers.savedir as string;
 	const projectsToDownload = (selectedProjectAnswers.projects as number[]).map(
 		(projectId: number) => projects.find((project) => project.id === projectId)
 	);
@@ -61,7 +98,15 @@ async function main() {
 			getProjectVersion(token, project.id, version.version_number).then(
 				(data) => {
 					if (data != null) {
-						saveData(data, path.join(basePath, project.location, project.name));
+						saveData(
+							data,
+							path.join(
+								basePath,
+								project.location,
+								project.name,
+								'version' + version.version_number.toString()
+							)
+						);
 					}
 				}
 			);
